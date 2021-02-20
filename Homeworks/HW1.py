d1e85ea65d3dfaadc4eeabf2cdc3317f2e28ce76
uataha <63406583+uataha@users.noreply.github.com>
2021-02-20 12:33:16 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,19 @@
-#Explain your work
+import random
 
-#Question 1
-for x in range(a):
-	print(a)
+
+
+dizi = []
+for x in range(1,100):
+    for asal in range(2,x):
+        if (x % asal) == 0:
+            break
+        elif(x % asal !=0) and (asal == x-1):
+            dizi.append(x)
+
+
+numaralar=(random.sample(dizi,3),
+           random.sample(dizi,3),
+           random.sample(dizi,3))
+for i in numaralar:
+    print(i)
+
