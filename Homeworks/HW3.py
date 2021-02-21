b756cae9b57368308d592989c8e16e23cf6ef4c5
uataha <63406583+uataha@users.noreply.github.com>
2021-02-21 15:33:05 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,33 @@
-#Explain your work
+sayılar1 = range(2,1000)
+def prime_first(param1):
 
-#Question 1
-for x in range(a):
-	print(a)
+    bolundu = False
+    asal_sayı_liste = []
+    for i in param1:
+        bolundu = False
+        for j in range(2,i):
+            if i%j == 0:
+                bolundu = True
+        if bolundu == False:
+            asal_sayı_liste.append(i)
+    return asal_sayı_liste
+
+print(prime_first(range(sayılar1[0],sayılar1[500])))
+
+sayılar1 = range(2,1000)
+def prime_second(param2):
+
+    bolundu = False
+    asal_sayı_liste = []
+    for i in param2:
+        bolundu = False
+        for j in range(2,i):
+            if i%j == 0:
+                bolundu = True
+        if bolundu == False:
+            asal_sayı_liste.append(i)
+    return asal_sayı_liste
+
+print(prime_second(range(sayılar1[500],len(sayılar1))))
+
+
